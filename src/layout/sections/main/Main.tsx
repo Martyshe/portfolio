b43d927bf048b098f93b498b7ab5b7fc26ced6b2
Martyshe <sapognikmisha@gmail.com>
2025-04-23 12:01:33 +0200

    completed main section layout with text and photo

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import photo from '../../../assets/images/myPhoto.webp'
+import { FlexWrapper } from '../../../components/FlexWrapper';
+
+export function Main() {
+  return (
+    <StyledMain>
+      <FlexWrapper justify='space-around' align='center'>
+      <div>
+        <p>Hi 👋,</p>
+        <p>My name is</p>
+        <Name>Mykhailo Martyshev</Name>
+        <MainTitle>I am a Front-End Developer</MainTitle>
+      </div>
+
+      <Photo src={photo} alt="My photo" />
+      </FlexWrapper>
+    </StyledMain>
+  );
+}
+
+const StyledMain = styled.section`
+    min-height: 50vh;
+    background-color: #6e852373;
+`
+
+const Name = styled.h2`
+    
+`
+
+const MainTitle = styled.h1`
+    
+`
+
+const Photo = styled.img`
+    width: 350px;
+    height: 350px;
+    border-radius: 50%;
+    object-fit: cover;
+    align-self: center;
+`
